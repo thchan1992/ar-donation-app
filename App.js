00745a545aa41ce572ff41aea954e68a36c8b521
Han Chan <chantszhang@gmail.com>
2022-10-08 10:19:06 +0100

    added responsive button to show the charity suggestion

--- App.js
+++ App.js
@@ -5,28 +5,75 @@ import {
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
+  ViroBox,
+  ViroButton,
 } from '@viro-community/react-viro';
 
 const HelloWorldSceneAR = () => {
   const [text, setText] = useState('Initializing AR...');
 
+  const [scence, setScence] = useState('');
+
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
-    if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
-    } else if (state === ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
+    // if (state === ViroConstants.TRACKING_NORMAL) {
+    setText('Hello World!');
+    // } else if (state === ViroConstants.TRACKING_NONE) {
+    //   // Handle loss of tracking
+    // }
   }
-
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
+      <ViroButton
+        source={require('./asset/homeless.png')}
+        position={[1, 0, -3]}
+        height={0.5}
+        width={1}
+        onClick={() => {
+          setScence('homeless');
+        }}
       />
+      {scence == 'homeless' && (
+        <ViroText
+          text={'We suggestion shelter'}
+          position={[1, 0.5, -3]}
+          style={{color: 'red'}}
+        />
+      )}
+      <ViroButton
+        source={require('./asset/refugees.png')}
+        position={[0, 0, -3]}
+        height={0.5}
+        width={1}
+        onTap={this._onButtonTap}
+        onClick={() => {
+          setScence('refugees');
+        }}
+      />
+      {scence == 'refugees' && (
+        <ViroText
+          text={'We suggestion refugee action'}
+          position={[0, 0.5, -3]}
+          style={{color: 'red'}}
+        />
+      )}
+      <ViroButton
+        source={require('./asset/orphan.png')}
+        position={[-1, 0, -3]}
+        height={0.5}
+        width={1}
+        onTap={this._onButtonTap}
+        onClick={() => {
+          setScence('orphan');
+        }}
+      />
+      {scence == 'orphan' && (
+        <ViroText
+          text={'We suggestion Orphans in Need'}
+          position={[-1, 0.5, -3]}
+          style={{color: 'red'}}
+        />
+      )}
     </ViroARScene>
   );
 };
