63fb86bcd25273eab94cc5549bb3a0a7b97475bc
Han Chan <chantszhang@gmail.com>
2022-11-02 23:49:54 +0000

    created styling

---
+++ util/styling.js
@@ -0,0 +1,46 @@
+export const headerColor = '#FF304F';
+
+export const bgColor = '#002651';
+
+export const buttonColor = '#28C7FA';
+
+export const borderColor = '#FF304F';
+
+export const instrucText = {
+  instrucText: {
+    alignSelf: 'center',
+    padding: 5,
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'white',
+  },
+};
+
+export const userSpeech = {
+  userSpeech: {
+    alignSelf: 'center',
+    padding: 5,
+    fontSize: 17,
+    color: '#775ADA',
+  },
+};
+
+export const image = {
+  image: {
+    paddingTop: 40,
+    width: 250,
+    height: 250,
+    borderWidth: 3,
+    borderColor: borderColor,
+    borderRadius: 8,
+    padding: 30,
+  },
+};
+
+export const infoText = {
+  infoText: {
+    padding: 10,
+    fontSize: 30,
+    color: 'white',
+  },
+};
